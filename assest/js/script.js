6c8070e76a4909e35da38e147ffc0bb3069b7487
David Effanga <57865396+xaviaxalee@users.noreply.github.com>
2024-02-07 12:33:06 +0100

    Add files via upload
    complete portfolio on property site. front end only

---
+++ assest/js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
